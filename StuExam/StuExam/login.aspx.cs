bb1d713e803d92bb6786742ae45f95a211461c54
hgf1996 <378938941@qq.com>
2017-04-19 21:57:46 +0800

    test

--- StuExam/StuExam/login.aspx.cs
+++ StuExam/StuExam/login.aspx.cs
@@ -11,7 +11,50 @@ namespace StuExam
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ///////
+
+        }
+
+        protected void btXs_Click(object sender, EventArgs e)
+        {
+            if (username.Text.Trim().Length == 8)
+            {
+                if (StuExam.DAL.Teacher.Exists(username.Text.Trim(), password.Text.Trim()) == 0)
+                {
+                    Error1.Text = "账号或者密码错误";
+                    return;
+                }
+                else
+                {
+                    Session["teacher_Number"] = username.Text.Trim();//存储老师的工号
+                    StuExam.Model.Teacher model = new StuExam.Model.Teacher();
+                    model = StuExam.DAL.Teacher.GetModel(username.Text.Trim(), password.Text.Trim());
+                    Session["teacher_Name"] = model.Name;//获取该教师的名字
+                    Response.Redirect("");
+                }
+            }
+            else if (username.Text.Trim().Length == 12)
+            {
+                if (StuExam.DAL.Student.Exists(username.Text.Trim(), password.Text.Trim()) == 0)
+                {
+                    Error1.Text = "账号或者密码错误";
+                    return;
+                }
+                else
+                {
+                    Session["student_Number"] = username.Text.Trim();//存储学生的学号
+                    StuExam.Model.Student model = new StuExam.Model.Student();
+                    model = StuExam.DAL.Student.GetModel(username.Text.Trim(), password.Text.Trim());
+                    Session["student_Profession"] = model.Profession;//获取该生的专业
+                    Session["student_Class"] = model.Class;//获取该生的专业班级
+                    Session["student_Name"] = model.Name;//获取该生的名字
+                    Response.Redirect("");
+                }
+            }
+            else
+            {
+                Error1.Text = "请输入12位学号或者8位工号";
+                return;
+            }
         }
     }
 }
